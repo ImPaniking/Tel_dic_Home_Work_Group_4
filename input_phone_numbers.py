34724df23c3c0a64e1d50bd359195de3f325d3e4
artiom <artiom.penschii91@gmail.com>
2022-07-29 22:47:33 +0300

    Простой метод перевода вводимых показателей в строку с разделителем + словари

--- input_phone_numbers.py
+++ input_phone_numbers.py
@@ -11,30 +11,50 @@
 # |Год рождения| date | |
 # |комментарий| str | |
 
-from email.policy import default
+import dictionarys as Dic
 
 
 def phone_number_row_creation \
     (
-        First_name = "", 
-        Last_name = "", 
-        Other_name = "",
-        Other_name_info = "", 
-        Company_name = "",
-        type_of_number = 1 ,
-        sex = 1 ,
-        bithd_day = "",
-        Commentary = "" 
+        First_name = " ", 
+        Last_name = " ", 
+        Other_name = " ",
+        type_of_contact = 0, 
+        Company_name = " ",
+        type_of_number = 0 ,
+        tel_number = " ",
+        email = " ",
+        sex = 0 ,
+        bithd_day = " ",
+        Commentary = " " ,
+        fav_check = False
     ):
-    row_info = [, , , , , , , , ,]
-    match len(First_name.split(" ")):
-            case 0:
-                continue
-            case 1:
-                First_name = list(First_name.split(" "))[0]
-            case 2:
-                Last_name = list(First_name.split(" "))[1]
-            case 3:
-                Other_name = list(First_name.split(" "))[2]
-            default:
-                
+    """Метод - принимает на вход параметры телефонного справочника, если они не заданы - использует значения по умолчанию
+
+    Returns:
+        str: на выходе строка , полей с разделителем '|'
+    """
+
+    ID_number = 1
+    colum_dic = \
+        {
+            0 : ID_number ,
+            1 : First_name ,
+            2 : Last_name , 
+            3 : Other_name ,
+            4 : Dic.type_of_contact_dic[type_of_contact] , 
+            5 : Company_name ,
+            6 : Dic.tel_type_dic[type_of_number] ,
+            7 : email ,
+            8 : tel_number ,
+            9 : Dic.sex_dic[sex] ,
+            10 : bithd_day ,
+            11 : Commentary ,
+            12 : fav_check
+        }
+    return '|'.join(map(str , colum_dic.values()))
+
+
+print(phone_number_row_creation())
+
+
