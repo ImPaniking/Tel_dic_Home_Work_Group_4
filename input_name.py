373a0b35927eb11ea8478bd23a430b0679699c64
PletenkovAndrey <and1257272@mail.ru>
2022-07-31 21:21:33 +0300

    add_input_name

---
+++ input_name.py
@@ -0,0 +1,15 @@
+from worck_with_base import take_from_base as TAKE
+from Look_up_delete_add import look_up_by_name
+from worck_with_base import row_creation_fun
+from worck_with_base import rewrite_base
+from Look_up_delete_add import look_up_by_name
+
+First_name = input("Ведите имя контакта "(name))
+Last_name = input("Введите фамилию "(sir_name)) 
+Other_name = input("Введите отчество "(Other_name))
+sex = input("Выберете пол") 
+tel_number = input("Введите номер телефона "(tel_number))
+
+For i,itme in tel_row.items():
+print(f"{i} - > {itme}")
+    if 
