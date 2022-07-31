a7372adc04359f7af3f1e0190ab30d41719b0808
Sergei <energostuk@gmail.com>
2022-07-31 14:48:11 +0300

    Add output card

---
+++ data_output.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+window = Tk()
+window.title('Информационная карточка')
+
+titles_list = ['Фамилия:', 'Имя:', 'Отчество:', 'Прочее (ник или лишние слова):', \
+               'Компания:', 'Тип:', 'Телефон', 'Пол', \
+               'Год рождения', 'Комментарий']
+
+for i in range(len(titles_list)):
+    for y in range(2):
+        if y == 0:
+            Message(width=300, text=titles_list[i]) \
+                .grid(row=i, column=y, sticky=W)
+        else:
+            Message(text='1') \
+                .grid(row=i, column=y)
+
+
+window.mainloop()
