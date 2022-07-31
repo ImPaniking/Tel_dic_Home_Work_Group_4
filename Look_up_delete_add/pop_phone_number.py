a84454ce9daebe564e4cd0244c7b59191244e791
artiom <artiom.penschii91@gmail.com>
2022-07-31 13:55:46 +0300

    Изменил разнос файлов что бы убрать ошибку цикличности

--- Look_up_delete_add/pop_phone_number.py
+++ Look_up_delete_add/pop_phone_number.py
@@ -1,10 +1,10 @@
 # Код удаления строчки из записной книжки
-import json
-from worck_with_base import rewrite_base as RE
-from worck_with_base import take_from_base as TAKE
+
+from worck_with_base import rewrite_base
+from worck_with_base import take_from_base
 
 def pop_row_number(ID_number : str):
-    phone_dic = TAKE()
+    phone_dic = take_from_base()
     phone_dic.pop(ID_number)
-    RE()
+    rewrite_base(phone_dic)
     
--- worck_with_base/__init__.py
+++ worck_with_base/__init__.py
@@ -1,4 +1,3 @@
 from .dic_of_numbers_creation import phone_number_row_creation
-from .pop_phone_number import pop_row_number
 from .take_incert_in_base import take_from_base
 from .take_incert_in_base import  rewrite_base
