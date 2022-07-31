8aefc5a7f9834f81472771d883ba0cc37b835b60
PletenkovAndrey <and1257272@mail.ru>
2022-08-01 01:12:23 +0300

    add

--- input_name.py
+++ input_name.py
@@ -4,31 +4,50 @@ from worck_with_base import row_creation_fun
 from worck_with_base import rewrite_base
 from Look_up_delete_add import look_up_by_name
 
-First_name = input("\nВедите имя контакта: ")
-
-# def GetRepearFname(tel_row)
-
-def look_up_by_name(name , data : dict):
-    return dict(filter(lambda x : x[1]["First name"] == name , data.items()))
-        
-
-Last_name = input("\nВведите фамилию: ") 
-
-# def GetRepearLname(tel_row)
-
-def look_up_by_name_sirname(name , sir_name, data : dict):
-        return dict(filter(lambda x : x[1]["First name"] == name and x[1]["Second name"] == sir_name  , data.items()))
-
-Other_name = input("\nВведите отчество: ")
-
-sex = input("\nВыберете пол: ") 
-for i,itme in sex_dic.items();
-    print(f"{i} - > {itme}")
-
-Type_of_Contact = input("\n")
-for i,itme in type_of_contact_dic.items();
-    print(f"{i} - > {itme}")
-
-tel_number = input("\nВведите номер телефона: ")
+def FirstName():
+    # for i,itme in tel_row.items():
+    #         print(f"{i} - > {itme}")
+            First_name = input("\nВедите имя контакта: ")
+            data = look_up_by_name(name , TAKE())
+            if len(data) < 1:
+                print("нет такого пользователя")
 
 
+
+def LastName():        
+    # for i,itme in tel_row.items():
+        # print(f"{i} - > {itme}")
+        # print("Выберите действие: ")
+        Last_name = input("\nВведите фамилию: ") 
+
+
+
+def OtherName():
+    # for i,itme in tel_row.items():
+    #         print(f"{i} - > {itme}")
+    #         print("Выберите действие: ")
+            Other_name = input("\nВведите отчество: ")
+
+def sex_orient(): 
+    for i,itme in sex_dic.items():
+        print(f"{i} - > {itme}")
+        sex = input("\nВыберете пол: ")
+
+def TypeOfContact():
+    for i,itme in type_of_contact_dic.items():
+        print(f"{i} - > {itme}")
+        print("Выберите тип: ")
+        Type_of_Contact = input("\nВыберите тип из списка: ")
+
+def TelNumber():
+    # for i,itme in tel_row.items():
+    #     print(f"{i} - > {itme}
+    #     print("Выберите действие: ")
+        tel_number = input("\nВведите номер телефона: ")
+
+
+# FirstName()
+# LastName()
+# sex_orient()
+# TypeOfContact()
+# TelNumber()
